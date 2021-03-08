2a4e635150b2ea50c6268a111b6b3c3b4b1ee103
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-08 14:32:26 +0100

    put and delete

--- routers/courses.js
+++ routers/courses.js
@@ -15,8 +15,34 @@ router.get('/:id', async (req,res)=>{
 
 router.post('', async (req,res)=>{
     let course = new Course(_.pick(req.body,'title','author','tags','price','isPublished'));
-    course = await course.save();
+    try {
+        course = await course.save();
+    } catch (error) {
+        res.status(400).send("Error in DB Store : "+error.message)
+    }
+    
     res.send(course);
 })
+
+router.put('/:id', async (req,res)=>{
+    let course =await Course.findById(req.params.id);
+    if(!course)
+        return res.status(404).send('Id is not found')
+    /* if(req.body.title)
+        course.title = req.body.title
+    if(req.body.author)
+        course.author = req.body.author */
+    course = _.merge(course,req.body);
+    course = await course.save();
+    res.send(course);
+});
+
+router.delete('/:id', async (req,res)=>{
+    let course =await Course.findById(req.params.id);
+    if(!course)
+        return res.status(404).send('Id is not found')
+    await Course.deleteOne({_id:req.params.id})
+    res.send(course);
+});
 
 module.exports = router
