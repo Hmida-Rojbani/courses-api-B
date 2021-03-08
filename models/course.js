7e42c71aec43ac55d4f405f3cb757271ca9862f0
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-08 14:32:46 +0100

    mongoose validator

--- models/course.js
+++ models/course.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-    title : String,
+    title : {type:String, required : true},
     author: String,
-    tags : [String],
-    price : Number,
+    tags : {type:[String],  validate : { validator : function(v){
+        return v.length > 0
+    }
+        , message : "A course must have at least one tag"}},
+    price : {type : Number, required : function () {return this.isPublished}, min : 10, max :300},
     date : {type : Date, default : Date.now()},
     isPublished : Boolean
 });
